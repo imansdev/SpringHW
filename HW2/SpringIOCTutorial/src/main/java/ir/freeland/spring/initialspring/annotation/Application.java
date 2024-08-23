d2aa8a00ebd69287ee4f465f7c70da2b70fafb2c
ImanS007 <imanabc007@gmail.com>
2024-08-23 17:50:05 +0100

    ADD print method to initialspring-annotation project

--- HW2/SpringIOCTutorial/src/main/java/ir/freeland/spring/initialspring/annotation/Application.java
+++ HW2/SpringIOCTutorial/src/main/java/ir/freeland/spring/initialspring/annotation/Application.java
@@ -3,19 +3,23 @@ package ir.freeland.spring.initialspring.annotation;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class  Application {
+public class Application {
   @SuppressWarnings("resource")
-public static void main(String[] args) {
-    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_annotation.xml");
-    
-    
-    CalculationSetterInjection calculationSetterInjection = context.getBean(CalculationSetterInjection.class);
+  public static void main(String[] args) {
+    ApplicationContext context =
+        new ClassPathXmlApplicationContext("applicationContext_annotation.xml");
+
+
+    CalculationSetterInjection calculationSetterInjection =
+        context.getBean(CalculationSetterInjection.class);
     calculationSetterInjection.complexCalculation();
 
-    CalculationConstructorInjection calculationConstructorInjection = context.getBean(CalculationConstructorInjection.class);
+    CalculationConstructorInjection calculationConstructorInjection =
+        context.getBean(CalculationConstructorInjection.class);
     calculationConstructorInjection.complexCalculation();
-    
-    CalculationFiledInjection calculationFiledInjection = context.getBean(CalculationFiledInjection.class);
+
+    CalculationFiledInjection calculationFiledInjection =
+        context.getBean(CalculationFiledInjection.class);
     calculationFiledInjection.complexCalculation();
   }
 }
--- HW2/SpringIOCTutorial/src/main/java/ir/freeland/spring/initialspring/annotation/CalculationFiledInjection.java
+++ HW2/SpringIOCTutorial/src/main/java/ir/freeland/spring/initialspring/annotation/CalculationFiledInjection.java
@@ -7,14 +7,19 @@ import org.springframework.stereotype.Component;
 public class CalculationFiledInjection {
   @Autowired
   private Calculator calculator;
-  
+
   @Autowired
   private Display display;
+
+  @Autowired
+  private Print print;
 
   public void complexCalculation() {
     int result = calculator.plus(2, 3);
 
     display.output(String.format("2 + 3 = %d", result));
+
+    print.doPrint("Printing was done successfully!");
   }
 
 }
