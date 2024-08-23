41060a9ae13bb43da09a55caa02ef00bff985508
ImanS007 <imanabc007@gmail.com>
2024-08-23 17:39:37 +0100

    ADD print method to initialmanual project

--- HW2/SpringIOCTutorial/src/main/java/ir/freeland/spring/initialmanual/Application.java
+++ HW2/SpringIOCTutorial/src/main/java/ir/freeland/spring/initialmanual/Application.java
@@ -4,14 +4,16 @@ public class Application {
   public static void main(String[] args) {
     Calculator calculator = new Calculator();
     Display display = new Display();
+    Print print = new Print();
 
     CalculationSetterInjection calculationSetterInjection = new CalculationSetterInjection();
     calculationSetterInjection.setCalculator(calculator);
     calculationSetterInjection.setDisplay(display);
+    calculationSetterInjection.setPrint(print);
     calculationSetterInjection.complexCalculation();
 
-    CalculationConstructorInjection
-        calculationConstructorInjection = new CalculationConstructorInjection(calculator, display);
+    CalculationConstructorInjection calculationConstructorInjection =
+        new CalculationConstructorInjection(calculator, display, print);
     calculationConstructorInjection.complexCalculation();
   }
 }
